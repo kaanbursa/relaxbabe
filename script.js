8ae91f3348eb43ed1146697ecf8e2dae93328b60
kaan bursa <kaanbursa9@gmail.com>
2020-06-09 16:47:20 +0300

    relaxer

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 15000;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+const intervals = 5000
+var round = 0
+
+breathAnimation();
+
+function breathAnimation() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, intervals);
+  }, intervals);
+  
+  document.getElementById('counter').innerHTML = 'Round ' + round
+  round += 1
+}
+
+setInterval(breathAnimation, totalTime);
